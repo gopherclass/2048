c837ec861831ca332813afeb7a20aaa8b07781cc
gopherclass <gopherclass@gmail.com>
2022-05-18 03:21:31 +0900

    support fonts

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
         use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
         test: /\.ts$/i,
         use: "ts-loader",
       },
