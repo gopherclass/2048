79fecb964631b0173b2030f1e815e51d6ab16c28
gopherclass <gopherclass@gmail.com>
2022-05-18 04:25:38 +0900

    implement mc on web

--- index.js
+++
@@ -1,6 +0,0 @@
-import * as wasm from "internal-wasm";
-import "./style/main.css";
-import "./js/application.js"
-
-console.log(wasm.some_message("foo"));
-
--- js/application.js
+++ js/application.js
@@ -1,3 +1,5 @@
+import * as wasm from "internal-wasm";
+
 import Actuator from "./html_actuator.js";
 import GameManager from "./game_manager.js"
 import HTMLActuator from "./html_actuator.js";
@@ -7,5 +9,21 @@ import LocalStorageManager from "./local_storage_manager.js";
 
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  const game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  const source = new wasm.PCGSource(1n, 0n);
+  document.getElementById("run").addEventListener("click", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const r = wasm.walk(game.grid.typedArray(), source);
+    if (r == 0) {
+      game.move(3);
+    } else if (r == 1) {
+      game.move(1);
+    } else if (r == 2) {
+      game.move(2);
+    } else if (r == 3) {
+      game.move(0);
+    }
+  });
 });
+
